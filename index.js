68cdc14b93b84046c95f360c13e514a27f61f898
Martin <souzam@sonic.net>
2022-10-15 14:16:06 -0400

    commit tree 24493eccd123a9cc77a8a326a7bda3351134d859

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(username, userEmail){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: username,
+            email: userEmail,
+        }),
+    })
+    .then(res => res.json())
+    .then(res => {
+        const p = document.createElement("p").textContent = res.id
+        document.body.append(p)
+    })
+    .catch(message => {
+        const errMsg = document.createElement("div").textContent = message
+        document.body.append(errMsg)
+    })
+}
